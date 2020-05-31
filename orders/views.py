fa426884662c109df049868c284418fbff774bb6
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-31 20:07:55 +0100

    authentication set up

--- orders/views.py
+++ orders/views.py
@@ -1,27 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-
-# user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-
-# from django.contrib.auth import authenticate
-# user = authenticate(username='john', password='secret')
-# if user is not None:
-#     # A backend authenticated the credentials
-# else:
-#     # No backend authenticated the credentials
 
 def home(request):
     return render(request, 'orders/home.html')
 
 def register(request):
+
+    first = request.POST["first"]
+    surname = request.POST["surname"]
+    email = request.POST["email"]
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    # user = User.objects.create_user(first, surname, email, username, password)
+
     return render(request, 'orders/register.html')
 
 def log_in(request):
+
+    username = request.POST["un"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid username/password."})
+
     return render(request, 'orders/login.html')
 
 def log_out(request):
+
+    logout(request)
     return render(request, 'orders/login.html', {'message': 'Successfully logged out!'})
 
 def menu(request):
