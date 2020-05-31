0e4439679e5949339ffb73fb79e75a92ed449868
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-31 17:20:44 +0100

    base structure setup

--- orders/views.py
+++ orders/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Project 3: TODO")
+def home(request):
+    return render(request, 'orders/home.html')
