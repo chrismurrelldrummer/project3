61665e1321a9dd0c15c6b5934f3c4538575039c5
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-05-31 18:16:38 +0100

    page layouts & urls

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("register", views.register, name="register"),
+    path("login", views.log_in, name="login"),
+    path("logout", views.log_out, name="logout"),
+    path("menu", views.menu, name="menu"),
+    path("basket", views.basket, name="basket")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+# user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+
+# from django.contrib.auth import authenticate
+# user = authenticate(username='john', password='secret')
+# if user is not None:
+#     # A backend authenticated the credentials
+# else:
+#     # No backend authenticated the credentials
+
 def home(request):
     return render(request, 'orders/home.html')
+
+def register(request):
+    return render(request, 'orders/register.html')
+
+def log_in(request):
+    return render(request, 'orders/login.html')
+
+def log_out(request):
+    return render(request, 'orders/login.html', {'message': 'Successfully logged out!'})
+
+def menu(request):
+    return render(request, 'orders/menu.html')
+
+def basket(request):
+    return render(request, 'orders/basket.html')
