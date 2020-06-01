ca52a7f5c28d2d1ead9e030d15229b69353a90ee
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-06-01 15:51:20 +0100

    login, reg & db working

--- orders/views.py
+++ orders/views.py
@@ -3,42 +3,72 @@ from django.shortcuts import render, reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+
 def home(request):
     return render(request, 'orders/home.html')
 
+
 def register(request):
 
-    first = request.POST["first"]
-    surname = request.POST["surname"]
-    email = request.POST["email"]
-    username = request.POST["username"]
-    password = request.POST["password"]
+    if request.method == 'POST':
 
-    # user = User.objects.create_user(first, surname, email, username, password)
+        first = request.POST["first"]
+        surname = request.POST["surname"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password"]
 
-    return render(request, 'orders/register.html')
+        user = User.objects.create_user(username, email, password)
+
+        user.first_name = first
+        user.last_name = surname
+        user.save()
+
+        login(request, user)
+
+        return HttpResponseRedirect(reverse("menu"))
+
+    else:
+        return render(request, 'orders/register.html')
+
 
 def log_in(request):
 
-    username = request.POST["un"]
-    password = request.POST["password"]
-    user = authenticate(request, username=username, password=password)
+    if request.method == 'POST':
 
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse("home"))
+        username = request.POST["un"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "users/login.html", {"message": "Invalid username/password."})
+
     else:
-        return render(request, "users/login.html", {"message": "Invalid username/password."})
+        logout(request)
+        return render(request, 'orders/login.html')
 
-    return render(request, 'orders/login.html')
 
 def log_out(request):
 
     logout(request)
     return render(request, 'orders/login.html', {'message': 'Successfully logged out!'})
 
+
 def menu(request):
-    return render(request, 'orders/menu.html')
+
+    if request.user.is_authenticated:
+
+        context = {
+            "user": request.user
+        }
+        return render(request, "orders/menu.html", context)
+
+    else:
+        return render(request, "orders/menu.html")
+
 
 def basket(request):
     return render(request, 'orders/basket.html')
