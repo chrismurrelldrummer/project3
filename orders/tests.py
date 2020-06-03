d827b903a5258bb492b1671bd9baa46cb5dc4015
Chris Murrell <chrismurrelldrummer@gmail.com>
2020-06-03 12:09:54 +0100

    updated tests

--- orders/tests.py
+++ orders/tests.py
@@ -7,20 +7,20 @@ class ModelsTestCase(TestCase):
 
     def setUp(self):
 
-    # Create pizzas
-    p1 = Pizza.objects.create(typ='Regular', category='Cheese', size='small', price='12.70')
-    p2 = Pizza.objects.create(typ='Regular', category='2 topping', size='small', price='15.20')
-    p3 = Pizza.objects.create(typ='Regular', category='2 topping', size='large', price='21.95')
-    p4 = Pizza.objects.create(typ='Sicilian', category='3 item', size='large', price='44.70')
+        # Create pizzas
+        p1 = Pizza.objects.create(typ='Regular', category='Cheese', size='small', price='12.70')
+        p2 = Pizza.objects.create(typ='Regular', category='2 topping', size='small', price='15.20')
+        p3 = Pizza.objects.create(typ='Regular', category='2 topping', size='large', price='21.95')
+        p4 = Pizza.objects.create(typ='Sicilian', category='3 item', size='large', price='44.70')
 
-    # Create toppings
-    Toppings.objects.create(typ='Pepperoni')
-    Toppings.objects.create(typ='Sausage')
-    Toppings.objects.create(typ='Mushrooms')
-    Toppings.objects.create(typ='Onions')
-    Toppings.objects.create(typ='Ham')
-    Toppings.objects.create(typ='Canadian Bacon')
-    Toppings.objects.create(typ='Pineapple')
+        # Create toppings
+        Toppings.objects.create(typ='Pepperoni')
+        Toppings.objects.create(typ='Sausage')
+        Toppings.objects.create(typ='Mushrooms')
+        Toppings.objects.create(typ='Onions')
+        Toppings.objects.create(typ='Ham')
+        Toppings.objects.create(typ='Canadian Bacon')
+        Toppings.objects.create(typ='Pineapple')
 
     # test adding 2 topping to a pizza
     def test_toppings_add1(self):
